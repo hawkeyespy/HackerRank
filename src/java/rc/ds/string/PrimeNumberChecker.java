e6a6719ca54e2fc6c036ad9f87c2398bcb2db69b
hawkeyespy <hawk.eyespy@gmail.com>
2016-07-30 07:28:17 -0500

    Adding new classes in ds.string package.

---
+++ src/java/rc/ds/string/PrimeNumberChecker.java
@@ -0,0 +1,14 @@
+package rc.ds.string;
+
+public class PrimeNumberChecker {
+
+   public static Boolean validate(final Integer primeNumber) {
+      for (int i = 2; i < (primeNumber / 2); i++) {
+         if (primeNumber % i == 0) {
+            return false;
+         }
+      }
+      return true;
+   }
+
+}
